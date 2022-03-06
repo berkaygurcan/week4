47ca844b6a3001df7e74972a2e0c4675fbb768b3
Berkay Gürcan <46679861+berkaygurcan@users.noreply.github.com>
2022-03-06 15:35:16 +0300

    Filtreleme işlemi üzerinde düzenlemeler yapıldı

--- src/FilterTodo.tsx
+++ src/FilterTodo.tsx
@@ -64,7 +64,7 @@ export default function FilterTodo({
       .then(response => {
         console.log(response)
         console.log(response.data)
-        
+        setTodoList(response.data)
       });
   
   };
